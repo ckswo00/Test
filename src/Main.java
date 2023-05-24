83e9214174b17f5cdc83977157839a6442594a92
Poir <ckswo00@naver.com>
2023-05-24 22:46:39 +0900

    [2742] 브론즈4 2023-05-24

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -9,16 +8,11 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
-        int index = 0;
         int[] arr = new int[N];
-        while(st.hasMoreTokens()){
-            arr[index] = Integer.parseInt(st.nextToken());
-            index++;
-        }
 
-        Arrays.sort(arr);
-        System.out.println(arr[0]+" "+arr[N-1]);
+        for(int i=0;i<N;i++){
+            arr[i] = N-i;
+            System.out.println(arr[i]);
+        }
     }
 }
