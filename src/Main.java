3b524067f5f86781bd1fab2e79d900b4214fa3f1
Poir <ckswo00@naver.com>
2023-05-24 21:49:14 +0900

    [10818] 2023-05-24

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+        int[] arr = new int[count];
+
+        for(int i=0;i<count;i++){
+            arr[i] = in.nextInt();
+        }
+
+        in.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0]+" "+arr[count-1]);
     }
-}
+} // 10818
