80906eac51101315281a6e0da203299b62ecbecb
Poir <ckswo00@naver.com>
2023-05-24 22:26:57 +0900

    [10818] 2023-05-24 BufferedReader 연습

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
 import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-        Scanner in = new Scanner(System.in);
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = in.nextInt();
-        int[] arr = new int[count];
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        for(int i=0;i<count;i++){
-            arr[i] = in.nextInt();
+        int index = 0;
+        int[] arr = new int[N];
+        while(st.hasMoreTokens()){
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
         }
 
-        in.close();
         Arrays.sort(arr);
-        System.out.println(arr[0]+" "+arr[count-1]);
+        System.out.println(arr[0]+" "+arr[N-1]);
     }
-} // 10818
+}
