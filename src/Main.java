867ecef7c73e92802c692764b78222a013ec75e5
Poir <ckswo00@naver.com>
2023-05-25 18:23:49 +0900

    [2908] 브론즈2 상수 2023-05-25

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,35 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int[] arr = new int[2];
+        int index = 0;
 
-        for(int i=0;i<N;i++){
-            arr[i] = N-i;
-            System.out.println(arr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        while(st.hasMoreTokens()){
+            arr[index] = Integer.parseInt((st.nextToken()));
+            index++;
+        }
+
+        int num0 = arr[0];
+        int num1 = arr[1];
+
+        int answer0 = 0;
+        int answer1 = 0;
+
+        while(num0!=0){
+            answer0 = answer0*10+num0%10;
+            num0 /= 10;
+        }
+        while(num1!=0){
+            answer1 = answer1*10+num1%10;
+            num1 /= 10;
+        }
+
+        if(answer0<answer1){
+            System.out.println(answer1);
+        }else{
+            System.out.println(answer0);
         }
     }
 }
