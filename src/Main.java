ca462099754217475b72d10ab1df05c77c89441f
Poir <ckswo00@naver.com>
2023-05-26 19:10:02 +0900

    [2439] 브론즈4 별찍기2 2023-05-26

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
 
-        int[] arr = new int[2];
-        int index = 0;
-
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
-        while(st.hasMoreTokens()){
-            arr[index] = Integer.parseInt((st.nextToken()));
-            index++;
-        }
-
-        int num0 = arr[0];
-        int num1 = arr[1];
-
-        int answer0 = 0;
-        int answer1 = 0;
-
-        while(num0!=0){
-            answer0 = answer0*10+num0%10;
-            num0 /= 10;
-        }
-        while(num1!=0){
-            answer1 = answer1*10+num1%10;
-            num1 /= 10;
-        }
-
-        if(answer0<answer1){
-            System.out.println(answer1);
-        }else{
-            System.out.println(answer0);
+        for(int i=N;i>0;i--) {
+            for(int j=i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=N-i+1;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
